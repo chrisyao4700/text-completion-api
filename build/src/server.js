89ebfb08c8543f752ceaa1d628d90b66c0f78084
chrisyao4700 <chrisyao900908@gmail.com>
2023-02-06 19:38:23 -0800

    Remove build folder

--- build/src/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
-const port = process.env.PORT || 4010;
-app_1.app.listen(port, () => console.log(`Text Completion API listening at http://localhost:${port}`));
