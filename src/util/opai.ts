d065c4d6a4562cca5bcd709dbb3fe282519dd6fd
chrisyao4700 <chrisyao900908@gmail.com>
2023-02-08 21:21:59 -0800

    Add stable

--- src/util/opai.ts
+++ src/util/opai.ts
@@ -8,17 +8,22 @@ const openai = new OpenAIApi(configuration);
 
 export const createTextFromPrompt = async (prompt: string): Promise<string> => {
     // console.log('Sent out:',prompt);
-    if(process.env.TEXT_LOGGING === 'true') console.log('Sent out:',prompt);
-    const completion = await openai.createCompletion({
-        model: `${process.env.OPEN_API_USING_MODEL || 'texdt-davinci-003'}`,
-        prompt: prompt,
-        temperature: 1,
-        max_tokens: 2048
-    });
-    
-    // console.log(completion.data.choices);
-    const resText = `${completion.data.choices[0].text}`.split('\n').join('');
-    if(process.env.TEXT_LOGGING === 'true') console.log('Came in:', resText);
-    if(resText === '') throw new Error('Cannot get response');
-    return resText;
+    try {
+        if (process.env.TEXT_LOGGING === 'true') console.log('Sent out:', prompt);
+        const completion = await openai.createCompletion({
+            model: `${process.env.OPEN_API_USING_MODEL || 'texdt-davinci-003'}`,
+            prompt: prompt,
+            temperature: 1,
+            max_tokens: 2048
+        });
+
+        // console.log(completion.data.choices);
+        const resText = `${completion.data.choices[0].text}`.split('\n').join('');
+        if (process.env.TEXT_LOGGING === 'true') console.log('Came in:', resText);
+        if (resText === '') return 'Too hard to understand, please try again';
+        return resText;
+    } catch (e) {
+        console.log(e)
+        return 'Too hard to understand, please try again';
+    }
 }
