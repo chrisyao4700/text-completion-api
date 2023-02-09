ef07de9697f1b6be8721afc1cd418a8ed98132b6
chrisyao4700 <chrisyao900908@gmail.com>
2023-02-08 18:36:28 -0800

    Add optional logger

--- src/util/opai.ts
+++ src/util/opai.ts
@@ -8,15 +8,17 @@ const openai = new OpenAIApi(configuration);
 
 export const createTextFromPrompt = async (prompt: string): Promise<string> => {
     // console.log('Sent out:',prompt);
+    if(process.env.TEXT_LOGGING === 'true') console.log('Sent out:',prompt);
     const completion = await openai.createCompletion({
         model: `${process.env.OPEN_API_USING_MODEL || 'texdt-davinci-003'}`,
         prompt: prompt,
         temperature: 1,
         max_tokens: 2048
     });
+    
     // console.log(completion.data.choices);
     const resText = `${completion.data.choices[0].text}`.split('\n').join(' ');
-    // console.log('Came in:', resText);
+    if(process.env.TEXT_LOGGING === 'true') console.log('Came in:', resText);
     if(resText === '') throw new Error('Cannot get response');
     return resText;
 }
