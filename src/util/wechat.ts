fff3c239f557c8838db6fa59914514ff03f35c75
chrisyao4700 <chrisyao900908@gmail.com>
2023-02-11 11:56:31 -0800

    Save

--- src/util/wechat.ts
+++ src/util/wechat.ts
@@ -132,20 +132,22 @@ export const downloadWeChatMedia = async (mediaId: string, filePath: string): Pr
     const accessToken = await getWeChatAccessToken();
     const url = `https://api.weixin.qq.com/cgi-bin/media/get?access_token=${accessToken}&media_id=${mediaId}`;
 
-    const response = await axios({
-        url,
-        method: 'GET',
-        responseType: 'stream',
-    });
-
-    response.data.pipe(fs.createWriteStream(filePath));
-
     return new Promise((resolve, reject) => {
-        response.data.on('end', () => {
-            resolve();
-        });
-        response.data.on('error', (err: Error) => {
-            reject(err);
-        });
+        axios({
+            method: 'GET',
+            url: url,
+            responseType: 'stream'
+        })
+            .then(function (response) {
+                console.log(response);
+                response.data.pipe(fs.createWriteStream(filePath));
+                response.data.on('end', () => {
+                    resolve();
+                });
+
+                response.data.on('error', (err: Error) => {
+                    reject(err);
+                });
+            });
     });
 }
