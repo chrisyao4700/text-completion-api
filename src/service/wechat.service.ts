6f2361c484a588ad5362d28e785fb7ac02bb97d3
chrisyao4700 <chrisyao900908@gmail.com>
2023-02-11 16:20:47 -0800

    SAVE

--- src/service/wechat.service.ts
+++ src/service/wechat.service.ts
@@ -28,9 +28,9 @@ const startNewChat = async (chat: Chat, text: string): Promise<string> => {
     const prompt = `你的名字是 ${process.env.CHAT_AGENT_CHINESE_NAME}`+ 
     `(${process.env.CHAT_AGENT_ENGLISH_NAME})`+
     `请回复以下信息，\n`+
-    '/*信息开始*/'+
+    '*信息开始*'+
     `\n${text}\n`+
-    '/*信息结束*/';
+    '*信息结束*';
     const resText = await createTextFromPrompt(prompt);
     await chat.createLine({ text: text, role: LINE_ROLE.HUMAN });
     await chat.createLine({ text: resText, role: LINE_ROLE.AI });
@@ -45,12 +45,12 @@ const continueChat = async (chat: Chat, text: string): Promise<string> => {
     const prompt =  `你的名字是 ${process.env.CHAT_AGENT_CHINESE_NAME}`+ 
     `(${process.env.CHAT_AGENT_ENGLISH_NAME})`+
     `请基于聊天记录回复新信息，\n`+
-    '/*聊天记录开始*/'+
+    '*聊天记录开始*'+
     `\n${historyText}\n`+
-    '/*聊天记录结束*/'+
-    '/*新信息开始*/'+
+    '*聊天记录结束*'+
+    '*新信息开始*'+
     `\n${text}\n`+
-    '/*新信息结束*/';
+    '*新信息结束*';
     const resText = await createTextFromPrompt(prompt);
     await chat.createLine({ text: text, role: LINE_ROLE.HUMAN });
     await chat.createLine({ text: resText, role: LINE_ROLE.AI });
