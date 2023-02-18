5884b7dec141d574b0fa180cd25a60ae30aedd70
chrisyao4700 <chrisyao900908@gmail.com>
2023-02-17 21:23:56 -0800

    SAVE

--- src/auth/serviceAuth.ts
+++ src/auth/serviceAuth.ts
@@ -1,6 +1,6 @@
-import { AuthChecker } from '@animal-high-tech/animal-service-auth';
-const apiSecret = 'animal-arclight-2023';
-const authServices = ['animal-front'];
-const AuthCheckerMiddleware = new AuthChecker(apiSecret,authServices).build();
+// import { AuthChecker } from '@animal-high-tech/animal-service-auth';
+// const apiSecret = 'animal-arclight-2023';
+// const authServices = ['animal-front'];
+// const AuthCheckerMiddleware = new AuthChecker(apiSecret,authServices).build();
 
-export default AuthCheckerMiddleware;
+// export default AuthCheckerMiddleware;
